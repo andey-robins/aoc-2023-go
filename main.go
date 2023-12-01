d15b6a91d60d12b9445b0327006ea13248d5b191
Andey Robins <ja548335@ucf.edu>
2023-12-01 12:42:02 -0500

    Add glue to simplify each day

--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "github.com/andey-robins/aoc-2023-go/solutions"
+import (
+	"flag"
+	"fmt"
+
+	"github.com/andey-robins/aoc-2023-go/solutions"
+)
 
 func main() {
-	solutions.Day1()
+	var day int
+	flag.IntVar(&day, "day", 0, "day to run")
+	flag.Parse()
+
+	switch day {
+	case 1:
+		solutions.Day1()
+	case 2:
+		solutions.Day2()
+	case 3:
+		solutions.Day3()
+	case 4:
+		solutions.Day4()
+	case 5:
+		solutions.Day5()
+	case 6:
+		solutions.Day6()
+	case 7:
+		solutions.Day7()
+	case 8:
+		solutions.Day8()
+	case 9:
+		solutions.Day9()
+	case 10:
+		solutions.Day10()
+	case 11:
+		solutions.Day11()
+	case 12:
+		solutions.Day12()
+	case 13:
+		solutions.Day13()
+	case 14:
+		solutions.Day14()
+	case 15:
+		solutions.Day15()
+	case 16:
+		solutions.Day16()
+	case 17:
+		solutions.Day17()
+	case 18:
+		solutions.Day18()
+	case 19:
+		solutions.Day19()
+	case 20:
+		solutions.Day20()
+	case 21:
+		solutions.Day21()
+	case 22:
+		solutions.Day22()
+	case 23:
+		solutions.Day23()
+	case 24:
+		solutions.Day24()
+	case 25:
+		solutions.Day25()
+	default:
+		fmt.Println("no solution for that day yet")
+	}
 }
--- solutions/driver.go
+++ solutions/driver.go
@@ -27,3 +27,99 @@ func Day1() {
 	fmt.Printf("part1: %v\n", day01.Part1(string(input)))
 	fmt.Printf("part2: %v\n", day01.Part2(string(input)))
 }
+
+func Day2() {
+	// TODO: Implement Day2
+}
+
+func Day3() {
+	// TODO: Implement Day3
+}
+
+func Day4() {
+	// TODO: Implement Day4
+}
+
+func Day5() {
+	// TODO: Implement Day5
+}
+
+func Day6() {
+	// TODO: Implement Day6
+}
+
+func Day7() {
+	// TODO: Implement Day7
+}
+
+func Day8() {
+	// TODO: Implement Day8
+}
+
+func Day9() {
+	// TODO: Implement Day9
+}
+
+func Day10() {
+	// TODO: Implement Day10
+}
+
+func Day11() {
+	// TODO: Implement Day11
+}
+
+func Day12() {
+	// TODO: Implement Day12
+}
+
+func Day13() {
+	// TODO: Implement Day13
+}
+
+func Day14() {
+	// TODO: Implement Day14
+}
+
+func Day15() {
+	// TODO: Implement Day15
+}
+
+func Day16() {
+	// TODO: Implement Day16
+}
+
+func Day17() {
+	// TODO: Implement Day17
+}
+
+func Day18() {
+	// TODO: Implement Day18
+}
+
+func Day19() {
+	// TODO: Implement Day19
+}
+
+func Day20() {
+	// TODO: Implement Day20
+}
+
+func Day21() {
+	// TODO: Implement Day21
+}
+
+func Day22() {
+	// TODO: Implement Day22
+}
+
+func Day23() {
+	// TODO: Implement Day23
+}
+
+func Day24() {
+	// TODO: Implement Day24
+}
+
+func Day25() {
+	// TODO: Implement Day25
+}
